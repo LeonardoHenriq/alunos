56f643677eeef1231088473de72e7735e30097fc
Leonardo Urbano <leonardo.urbano@printwayy.com>
2023-03-09 14:25:02 -0300

    primeiro commit

---
+++ Alunos_app/Alunos_app/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alunos_app.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
